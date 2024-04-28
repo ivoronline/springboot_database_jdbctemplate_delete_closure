1b89121d1ab8d6cad98022279aaf235282fd3bef
ivoronline <ivoronline@gmail.com>
2024-04-28 21:29:34 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_database_jdbctemplate_delete_closure/SpringbootDatabaseJdbctemplateDeleteClosureApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_database_jdbctemplate_delete_closure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseJdbctemplateDeleteClosureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDatabaseJdbctemplateDeleteClosureApplication.class, args);
+  }
+
+}
